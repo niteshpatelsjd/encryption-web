4506b5bbd336d3594c74505cd9f52a06a2c63f89
niteshpatelsjd <niteshpatelsjd@gmail.com>
2026-09-02 16:15:26 +0530

    base url updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:6001/api/v1/'
+  import.meta.env.VITE_API_BASE_URL || 'https://encryption-api-b67f.onrender.com/api/v1/'
 
 export const publicApi = axios.create({
   baseURL: API_BASE_URL,
