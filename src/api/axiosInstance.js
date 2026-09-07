b5304d9effe948217392a3ed23cb2e6fbd21e584
niteshpatelsjd <niteshpatelsjd@gmail.com>
2026-09-07 21:56:29 +0530

    latest code updated

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'https://encryption-api-b67f.onrender.com/api/v1/'
+  import.meta.env.VITE_API_BASE_URL || 'https://api.highestencryptionphone.com/api/v1/'
 
 export const publicApi = axios.create({
   baseURL: API_BASE_URL,
